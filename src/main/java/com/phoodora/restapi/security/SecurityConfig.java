48424514576cb533940771d8121aa1fd54350406
Luukas <78479063+luukasmepham@users.noreply.github.com>
2021-11-30 17:24:54 +0200

    Update SecurityConfig.java

--- src/main/java/com/phoodora/restapi/security/SecurityConfig.java
+++ src/main/java/com/phoodora/restapi/security/SecurityConfig.java
@@ -28,6 +28,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         
+        http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
+
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
