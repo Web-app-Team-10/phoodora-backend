e8b6d3809a6566770bad32e49d13d605b36ea5c2
NikoManni <t0mani01@students.oamk.fi>
2021-12-01 10:25:15 +0200

    Added empty home page

--- src/main/java/com/phoodora/restapi/controllers/Controller.java
+++ src/main/java/com/phoodora/restapi/controllers/Controller.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -28,6 +29,12 @@ public class Controller {
 
     @Autowired
     UsersRepository usersRepository;
+
+    //EMPTY HOME PAGE
+        @RequestMapping("/")
+        String home(){
+            return "Welcome to the home page";
+        }
 
     // ORDER MAPPINGS
         @GetMapping("/admin/orders")
