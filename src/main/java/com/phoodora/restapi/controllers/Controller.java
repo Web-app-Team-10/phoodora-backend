12f6d38dd32dcbe369fb6d5d574b0d66b76fa14d
Luukas <78479063+luukasmepham@users.noreply.github.com>
2021-12-02 16:35:45 +0200

    Update Controller.java

--- src/main/java/com/phoodora/restapi/controllers/Controller.java
+++ src/main/java/com/phoodora/restapi/controllers/Controller.java
@@ -7,7 +7,7 @@ import com.phoodora.restapi.repositories.UsersRepository;
 import com.phoodora.restapi.services.AppService;
 
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class Controller {
 
