462b973272c9eddfb0f9ac85434dc4543a5c6dee
Luukas <78479063+luukasmepham@users.noreply.github.com>
2021-12-02 16:34:57 +0200

    Update Application.java

--- src/main/java/com/phoodora/restapi/Application.java
+++ src/main/java/com/phoodora/restapi/Application.java
@@ -2,8 +2,10 @@ package com.phoodora.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
@@ -12,6 +14,16 @@ public class Application {
     // public BCryptPasswordEncoder bCryptPasswordEncoder() {
     //     return new BCryptPasswordEncoder(); 
     // }
+
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/");
+			}
+		};
+	}
      
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
