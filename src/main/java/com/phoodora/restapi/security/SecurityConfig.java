332ee714fb138e26c7fce31c1603d42c88d85e8a
Luukas <78479063+luukasmepham@users.noreply.github.com>
2021-12-03 15:42:34 +0200

    cors

--- src/main/java/com/phoodora/restapi/security/SecurityConfig.java
+++ src/main/java/com/phoodora/restapi/security/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.phoodora.restapi.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -45,5 +51,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.addFilter(new AuthenticationFilter(authenticationManager()));
         http.addFilter(new AuthorizationFilter(authenticationManager()));
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() 
+    {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
