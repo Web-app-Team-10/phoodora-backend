d7cd87236d9db799a2db73095ebe4287f0fa8973
Luukas <78479063+luukasmepham@users.noreply.github.com>
2021-12-03 15:20:07 +0200

    cors

--- src/main/java/com/phoodora/restapi/security/SecurityConfig.java
+++ src/main/java/com/phoodora/restapi/security/SecurityConfig.java
@@ -27,7 +27,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.cors();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
