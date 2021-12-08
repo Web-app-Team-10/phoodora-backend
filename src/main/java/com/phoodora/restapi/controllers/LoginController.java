dbdb545f4c9168faffca57f7aedba17a39933586
Luukas <78479063+luukasmepham@users.noreply.github.com>
2021-12-08 13:33:58 +0200

    try7

--- src/main/java/com/phoodora/restapi/controllers/LoginController.java
+++ src/main/java/com/phoodora/restapi/controllers/LoginController.java
@@ -2,14 +2,14 @@ package com.phoodora.restapi.controllers;
 
 import com.phoodora.restapi.models.Users;
 
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.phoodora.restapi.security.UserDetails;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class LoginController {
@@ -17,13 +17,25 @@ public class LoginController {
     @Autowired
     private UserDetails userDetailsService;
 
-    @PostMapping(path = "/register/customer", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
-    public void addCustomer(Users user) {
-         userDetailsService.createUser(user, "CUSTOMER");
-    }
+    @PostMapping("/register/customer")
+    public void addCustomer(@RequestBody JSONObject input) {
+      String username = (String) input.get("username");
+      String password = (String) input.get("password");
 
-    @PostMapping(path = "/register/manager", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
-    public void addManager(Users user) {
+         Users user = new Users();
+         user.setUsername(username); 
+         user.setPassword(password);
+         userDetailsService.createUser(user, "CUSTOMER");
+   }
+
+    @PostMapping("/register/manager")
+    public void addManager(@RequestBody JSONObject input) {
+      String username = (String) input.get("username");
+      String password = (String) input.get("password");
+
+         Users user = new Users();
+         user.setUsername(username); 
+         user.setPassword(password);
          userDetailsService.createUser(user, "MANAGER");
-    }
+   }
 }
