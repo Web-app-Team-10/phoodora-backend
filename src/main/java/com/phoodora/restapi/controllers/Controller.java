1816b3c10c47e99b72a1a86ee148b76b404e7384
Luukas <78479063+luukasmepham@users.noreply.github.com>
2021-12-09 17:52:51 +0200

    hgf

--- src/main/java/com/phoodora/restapi/controllers/Controller.java
+++ src/main/java/com/phoodora/restapi/controllers/Controller.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
