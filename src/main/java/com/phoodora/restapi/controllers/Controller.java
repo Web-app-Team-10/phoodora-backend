65ce8dab01e547d70005c923e8ba4faa971e8297
Luukas <78479063+luukasmepham@users.noreply.github.com>
2021-12-09 16:29:35 +0200

    Changes

--- src/main/java/com/phoodora/restapi/controllers/Controller.java
+++ src/main/java/com/phoodora/restapi/controllers/Controller.java
@@ -9,10 +9,8 @@ import com.phoodora.restapi.models.Restaurant;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
-import org.yaml.snakeyaml.util.ArrayUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.security.core.context.SecurityContextHolder;
 
